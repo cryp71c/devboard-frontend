766c997ddac66dc4da0475138253a9db43b932c1
cryp71c <alexrapino75@gmail.com>
2025-07-20 17:15:49 -0500

    workflow update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  base: '/resume-devboard/', // your repo name
 });

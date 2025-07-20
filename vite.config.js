9615f871ba2c73cce11f455ad3a0be0b4a7b3531
cryp71c <alexrapino75@gmail.com>
2025-07-20 17:35:40 -0500

    workflow update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // or '/your-repo/' if deployed at subpath
   plugins: [react()],
-  base: '/',
+  build: {
+    outDir: 'dist',
+  }
 })

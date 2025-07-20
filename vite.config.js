f6b6885fa42eefe531388025f7ae91598b27bad2
cryp71c <alexrapino75@gmail.com>
2025-07-20 17:29:24 -0500

    workflow update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import { writeFileSync } from 'fs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-
-  buildEnd() {
-    writeFileSync(resolve(__dirname, 'dist/CNAME'), 'cryp71c.dev');
-  },
-});
+})

13016b7540d83cb4ff93b184e9033285496ec293
cryp71c <alexrapino75@gmail.com>
2025-07-20 17:47:41 -0500

    Having issues getting webpage to display.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/devboard-frontend/', // or '/your-repo/' if deployed at subpath
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
